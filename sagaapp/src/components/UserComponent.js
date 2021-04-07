db474124ab690a295ad62bc02e87a0e1edfcda75
Raskolnikov <jerry.tann@opteyes.cn>
2021-04-07 10:51:03 +0800

    feat: add template for counter

--- sagaapp/src/components/UserComponent.js
+++ sagaapp/src/components/UserComponent.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default function UserComponent(props) {
-    return (
-        <div>i am the user component</div>
-    )
+  return (
+    <div>
+      i am the user component
+      <div>
+        Basic Counter
+        <div>currentCount</div>
+        <button>+</button>
+        <button>-</button>
+      </div>
+    </div>
+  );
 }
