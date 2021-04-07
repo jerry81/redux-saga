418a9fe9fd618cb8884925e15d76e51b9880a019
Raskolnikov <jerry.tann@opteyes.cn>
2021-04-07 11:22:20 +0800

    feat: add redux

--- sagaapp/src/components/UserComponent.js
+++ sagaapp/src/components/UserComponent.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { connect } from "react-redux"; // connect api
 
 export default function UserComponent(props) {
+  function increment() {
+      console.log('increment')
+  }
+  function decrement() {
+      console.log('decrement')
+  }
   return (
     <div>
       i am the user component
       <div>
         Basic Counter
         <div>currentCount</div>
-        <button>+</button>
-        <button>-</button>
+        <button onClick={() => { increment() }}>+</button>
+        <button onClick={() => { decrement() }}>-</button>
       </div>
     </div>
   );
---
+++ sagaapp/src/redux/store.js
@@ -0,0 +1,32 @@
+
+import { createStore } from "redux";
+
+const initialState = {
+  /* not exported */
+  count: 0
+};
+
+const counter = function(state = initialState, action) {
+  // reducer
+  switch (
+    action.type // support multiple actions
+  ) {
+    case "INC": {
+      // allIds push, byIds push
+      return {
+        ...state, // updates object
+        count: ++state.count
+      };
+    }
+    case "DEC": {
+      return {
+        ...state,
+        count: --state.count
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export default createStore(counter)
